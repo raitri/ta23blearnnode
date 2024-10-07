fbb53872705707a2daca75d299aba282599d1832
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-07 11:22:05 +0300

    Explain es vs commonjs

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ import common from './common.js';
 common.hello();
 import es from './es.js';
 es.hello();
+
+// const common = require('./common.js');
+// common.hello();
+// const es = require('./es.js').default;
+// es.hello();
