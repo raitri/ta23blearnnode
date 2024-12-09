fd9d0474d0ea0c152f57a9df99f0d7b7dd37affd
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-12-09 11:00:36 +0200

    Backwords letter

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
 import { ref } from 'vue';
 
 let message = ref('Hello Vue!');
-setTimeout(() => {
-    message.value = 'Hello TA23b!';
-    console.log('message changed');
-}, 10_000);
+
 </script>
 <template>
-    <div class="container">
+    <div class="container mt-3">
         <div class="content">
-            <h1>{{ message }}</h1>
+            <button class="button is-primary" @click="message='Hello TA23B!'">Click me!</button>
+            <input class="input my-1" v-model="message">
+            <h1>{{ message.split('').reverse().join('') }}</h1>
+
         </div>
     </div>
 </template>
