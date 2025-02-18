24c2efb42cb41823bb950ec7be60758ef0da7eae
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-18 13:43:07 +0200

    Add axios example

--- src/pages/ChuckNorris.vue
+++ src/pages/ChuckNorris.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { ref } from 'vue';
-
+import axios from 'axios';
 
 let joke = ref('');
 
-let response = await fetch('https://api.chucknorris.io/jokes/random');
-let data = await response.json();
-joke.value = data.value;
+let response = await axios.get('https://api.chucknorris.io/jokes/random');
+joke.value = response.data.value;
+// axios.get('https://api.chucknorris.io/jokes/random').then(res => {
+//     joke.value = res.data.value;
+// });
 
 </script>
 
