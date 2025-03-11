7fb7bcb3a77a40255215dda5079397ca1eb45ce3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-03-11 13:22:55 +0200

    Add infinite scroll example

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
 import PagedPagination from '../components/PagedPagination.vue';
 
@@ -12,7 +12,8 @@ await page(currentPage.value);
 async function getCharacters(url) {
     let response = await axios.get(url);
     console.log(response.data);
-    characters.value = response.data.results;
+    characters.value.push(...response.data.results);
+    console.log(characters.value);
     pagination.value = response.data.info;
 }
 
@@ -31,10 +32,21 @@ async function page(page){
     await getCharacters('https://rickandmortyapi.com/api/character?page=' + page);
 }
 
+onMounted(() => {
+    document.addEventListener('scroll', () => {
+        if(window.scrollY+window.innerHeight > document.body.clientHeight-300 ) {
+            if(pagination.value.next){
+                next()
+            }
+            
+        }
+        //console.log(document.body.clientHeight, window.scrollY+window.innerHeight);
+    });
+});
+
 </script>
 
 <template>
-    <PagedPagination :pagination="pagination" :current="currentPage" @next="next" @prev="prev" @page="page"></PagedPagination>
     <div class="columns is-multiline">
         <div class="column is-one-quarter" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
