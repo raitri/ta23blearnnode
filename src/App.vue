09a3870b715c3823f12fb6a4fa46f43913a1c162
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-03-18 13:41:12 +0200

    Add cookie clicker

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@ import RouterTabs from './components/RouterTabs.vue';
 </script>
 <template>
     <RouterTabs></RouterTabs>
-    <div class="container">
+    <div class="container" v-if="$route.meta.container === undefined">
         <section class="section">
             <Suspense>
                 <RouterView></RouterView>
             </Suspense>
         </section>
     </div>
+    <Suspense v-else>
+        <RouterView></RouterView>
+    </Suspense>
 </template>
